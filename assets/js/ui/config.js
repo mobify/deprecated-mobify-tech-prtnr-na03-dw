192405f42abb1bf157336e64b0d6431d2474c339
Dan Harrison <hwyfour@gmail.com>
2014-12-09 13:13:22 -0800

    added base pikabu

--- assets/js/ui/config.js
+++ assets/js/ui/config.js
@@ -1,6 +1,7 @@
 require.config({
     'paths': {
         'fastclick': '../../../bower_components/fastclick/lib/fastclick',
+        'pikabu': '../../../bower_components/pikabu/build/pikabu.min',
         'deckard': '../../../bower_components/deckard/dist/deckard.min',
         'pre-tag': '../../../bower_components/pre-tag/index'
     }
--- assets/js/ui/view-scripts/global.js
+++ assets/js/ui/view-scripts/global.js
@@ -1,14 +1,24 @@
 define([
     '$',
     'fastclick',
+    'pikabu',
     'deckard'
 ],
-function($, fastclick) {
+function($, fastclick, Ppikabu) {
 
     var globalUI = function() {
         fastclick.attach(document.body);
 
         // Add any scripts you would like to run on ALL pages here
+
+        // Pikabu menu slideout
+        var pikabu = new Pikabu({
+        	viewportSelector: '.m-pikabu-viewport',
+            widths: {
+                left: '80%',
+                right: '80%'
+            }
+        });
     };
 
     return globalUI;
